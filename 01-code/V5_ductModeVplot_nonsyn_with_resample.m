3394188869919564e9a6bc076649da538472e922
jiaqiwang969 <jiaqiwang969@gmail.com>
2022-05-18 15:32:22 +0800

    add reference sensor effect

--- 01-code/V5_ductModeVplot_nonsyn_with_resample.m
+++ 01-code/V5_ductModeVplot_nonsyn_with_resample.m
@@ -11,13 +11,14 @@ chemin = '../database/01-rotateMic';
 
 %% add Basic parameters
 
-zH = 0.4;         % 测试距离
-nk = 12;          % 传声器的数量
-NumSM= 30;        % 测量的次数
-a=0.185;          % 管道半径
-S=pi*a^2;         % 管口面积
-Fs = 102400 ;     % 采样频率
-time=5;           % 采样时间
+zH = 0;             % 测试距离
+zRef=0.08;          % 参考传声器到测试面的轴向距离
+NumMic = 12;        % 传声器的数量
+NumSM  = 30;        % 测量的次数
+Radius = 0.2;       % 管道半径
+Area = pi*Radius^2; % 管口面积
+Fs = 102400;        % 采样频率
+time = 5;           % 采样时间
 
 rotor_speed=10000;              %轴转速信息
 
@@ -34,7 +35,7 @@ round=7;                        %分段,生成多个block，每个block 为round
 Ind = [1:NumSM];   %设定循环次数
 Num_file = Ind ;
 for i_file =Num_file
-    eval(['load ''',chemin,'/','RotaryTest-10000-Rotate-No-',num2str(i_file),'.mat''']);       %读取数据
+    eval(['load ''',chemin,'/','RotaryTest-',num2str(rotor_speed),'-Rotate-No-',num2str(i_file),'.mat''']);       %读取数据
     Tdata{i_file}=Data(:,1:13);
     % Step01: 通过key signal将其分段,生成多个block，每个block 为 round, 历时 25 秒。
     % 在这里需要增加等角度采样的操作：
@@ -57,43 +58,40 @@ data_tonal_rms2=mat2cell(data_tonal_rms,data_resample_interval(1)*ones(NumSM,1),
 data_tonal=kron(ones(cut_number,1),cell2mat(data_tonal_rms2));
 data_broadband=cell2mat(data_block)-data_tonal;
 
+%% 是否将参考传声器植入到互功率谱矩阵中
+if_consider_ref=0
 %% CPSD and phase
-Ind = [1:NumSM];   %设定循环次数
-Num_file = Ind ;
-CC1=[];
-for i_file =Num_file
-    temp_data=data_all(:,(1+(i_file-1)*13):(1+(i_file)*13-2));
-    T1=  kron(ones(1,12), temp_data  );
-    T2=  kron(temp_data, ones(1,12)  );
-    [temp,freq]=cpsd(T1,T2,Wind,Noverlap,Nfft,Fs);
-    CC1=[CC1 temp];
-end
-
+    Ind = [1:NumSM];   %设定循环次数
+    Num_file = Ind ;
+    CC1=[];
+    for i_file = Num_file
+        temp_data=data_all(:,(1+(i_file-1)*13):(1+(i_file)*13-2+if_consider_ref)); % 把传声器也考虑在内！！
+        T1=  kron(ones(1,12+if_consider_ref), temp_data  );
+        T2=  kron(temp_data, ones(1,12+if_consider_ref)  );
+        [temp,freq]=cpsd(T1,T2,Wind,Noverlap,Nfft,Fs);
+        CC1=[CC1 temp];
+    end
 % 重新生成CC矩阵,注意排序顺序，reshape([1:144],24,6) 代验证
-CC2=reshape(CC1,length(freq),144,30);
-CC3=reshape(CC2,length(freq),12,12,30);
+CC2=reshape(CC1,length(freq),(12+if_consider_ref)*(12+if_consider_ref),NumSM);
+CC3=reshape(CC2,length(freq),12+if_consider_ref,12+if_consider_ref,NumSM);
 amf=reshape(CC3(:,1,1,1),length(freq),1);
 % figure % 01-验证 & 用以找到最高点
 % plot(freq,amf);
-
-% 切面图
+%
 Freq_slice = [1];    %对应1xBPF
 df =freq(2) - freq(1);
 f0=rotor_speed/60*29*Freq_slice;
 
 
-% h=figure('Visible', 'on');
 for k=1:length(Freq_slice)
     xunhao_around=floor(f0(k)/df)+[-3:3];
     xuhao=xunhao_around(1)+find(amf(xunhao_around)==max(amf(xunhao_around)))-1;
     CC=[];
-    for i_file=1:30
-        CC = blkdiag(CC,reshape(CC3(xuhao,:,:,i_file),12,12));
+    for i_file=1:NumSM
+        CC = blkdiag(CC,reshape(CC3(xuhao,:,:,i_file),12+if_consider_ref,12+if_consider_ref));
     end
 end
-% imagesc(abs(CC));
-% axis equal
-
+figure;imagesc(abs(CC));axis equal
 
 %% 选择管道内可传播的模态%%%%%%%%
 % Kappa：无坐标系，r=1，有坐标系
@@ -102,28 +100,40 @@ f0=f0(1);
 omega=2*pi*f0; % 角速度
 k=omega/343;   % 波数
 load('Kappa.mat');    % 无流状态的管道声传播，其实是需要修正的（有流动的情况）
-Kappa = Kappa/a;
+Kappa = Kappa/Radius;
 Kappa=Kappa(:,1);     % 只考虑周向模态
 
 %% 传声器阵列%%%%% （柱坐标）
-mic_loc = zeros(NumSM*nk,3);
-XM=a*ones(nk*NumSM,1);
-YM=zeros(nk*NumSM,1);
+mic_loc = zeros(NumSM*(NumMic),3);
+XM=Radius*ones(NumSM*(NumMic),1);
+YM=zeros(NumMic*NumSM,1);
 for  j=1:NumSM
     theta1=[0:30:330]';
-    YM((1:nk)+(j-1)*nk,1)=theta1+(j-1)*1;
+    YM((1:NumMic)+(j-1)*NumMic,1)=theta1+(j-1)*1;
 end
 YM=YM/180*pi;
-ZM1=0.4*ones(nk,1);
+ZM1=0.4*ones(NumMic,1);
 for  j=1:NumSM
-    ZM((1:nk)+(j-1)*nk,1)=ZM1;
+    ZM((1:NumMic)+(j-1)*NumMic,1)=ZM1;
+end
+if if_consider_ref==1
+    XM=reshape([reshape(XM,12,30);Radius*ones(1,30)],  NumSM*(NumMic+if_consider_ref),1);
+    YM=reshape([reshape(YM,12,30);zeros(1,30)],        NumSM*(NumMic+if_consider_ref),1);
+    ZM=reshape([reshape(ZM,12,30);zRef*ones(1,30)],    NumSM*(NumMic+if_consider_ref),1);
 end
 mic_loc=[XM,YM,ZM];
 
-%% 根据截至频率，计算出可传播模态%
+figure
+xs = mic_loc(:,1).*cos(mic_loc(:,2));ys=mic_loc(:,1).*sin(mic_loc(:,2));
+for i = 1:NumSM
+    plot3(xs((1:NumMic)+(i-1)*NumMic),ys((1:NumMic)+(i-1)*NumMic),mic_loc((1:NumMic)+(i-1)*NumMic,3),'.');
+    hold on;
+end
+
+%% 根据截至频率，计算出可传播模态
 mode_prop2=propagated_models(k,Kappa);  % 可传播模态
 [row,col] = size(mode_prop2);           % 可传播模态数量
-[G]=matrix_G_trial(mode_prop2,Kappa,k,a,mic_loc);
+[G]=matrix_G_trial(mode_prop2,Kappa,k,Radius,mic_loc);
 cond(G)
 
 %% 非同步测量空间基函数的确定
@@ -161,7 +171,7 @@ P=P_complex;
 q_re1=(G'*G)^-1*G'*P;   %03-30
 abs_q1=abs(q_re1); % pref=2e-5;
 % 按照G的模态顺序重新排序
-[mode_new,order_mode]=sort(mode_prop2(:,1)) 
+[mode_new,order_mode]=sort(mode_prop2(:,1))
 figure; bar(mode_new,abs_q1(order_mode))
 
 
